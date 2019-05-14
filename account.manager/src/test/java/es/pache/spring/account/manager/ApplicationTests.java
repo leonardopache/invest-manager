d48de38ddf64eac7f0a39eeb64879d4238ea7ad1
Leonardo <leonardo@pache.eng.br>
2019-05-14 17:06:02 +0200

    new project to account management

---
+++ account.manager/src/test/java/es/pache/spring/account/manager/ApplicationTests.java
@@ -0,0 +1,16 @@
+package es.pache.spring.account.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ account.manager/src/test/java/es/pache/spring/account/manager/AuthenticationUserTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package es.pache.spring.account.manager;
+
+import static org.junit.Assert.fail;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * All test related with Login and Logout user.
+ * 
+ * @author lmarquespache
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthenticationUserTest {
+	
+	public void login_success() {
+		fail();
+	}
+	
+	public void login_error() {
+		fail();
+	}	
+	
+	public void logout_success() {
+		fail();
+	}
+	
+	public void logout_error() {
+		fail();
+	}
+
+	
+}
