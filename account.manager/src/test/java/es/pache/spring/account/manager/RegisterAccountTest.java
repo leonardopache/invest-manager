cd55c0a0bfb65c41169de2dfedc634f5741fd5ee
Leonardo <leonardo@pache.eng.br>
2019-05-17 14:51:28 +0200

    updates

--- account.manager/src/test/java/es/pache/spring/account/manager/RegisterAccountTest.java
+++ account.manager/src/test/java/es/pache/spring/account/manager/RegisterAccountTest.java
@@ -4,16 +4,32 @@
 package es.pache.spring.account.manager;
 
 import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import es.pache.spring.account.manager.controller.requests.RegisterFormRequest;
 
 /**
  * All test related with register a new account.
  * 
- * tutorial: https://www.callicoder.com/spring-boot-security-oauth2-social-login-part-1/
+ * tutorial:
+ * https://www.callicoder.com/spring-boot-security-oauth2-social-login-part-1/
  * 
  * @author lmarquespache
  *
@@ -22,19 +38,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RegisterAccountTest {
-	
-	public void create_new_account_by_form() {
-		fail();
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void create_new_account_by_form() throws Exception {
+		RegisterFormRequest filled_form = fill_register_form();
+		String body = objectToJson(filled_form);
+
+		ResultActions result = this.mockMvc
+				.perform(post("/register-form").contentType(MediaType.APPLICATION_JSON).content(body)).andDo(print())
+				.andExpect(status().isOk());
 	}
-	
+
+	private String objectToJson(Object filled_form) throws IOException, JsonGenerationException, JsonMappingException {
+		ObjectMapper obj = new ObjectMapper();
+		return obj.writeValueAsString(filled_form);
+	}
+
+	private RegisterFormRequest fill_register_form() {
+		RegisterFormRequest formRequest = new RegisterFormRequest();
+		formRequest.setMail("teste@test.com");
+		formRequest.setName("Unit Tes");
+		formRequest.setPassword("MD5password");
+		return formRequest;
+	}
+
 	public void create_new_account_by_google() {
 		fail();
-	}	
-	
+	}
+
 	public void create_new_account_by_fb() {
 		fail();
 	}
-	
-	//TODO[register] test to register errors.
-	
+
+	// TODO[register] test to register errors.
+
 }
