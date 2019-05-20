15e70ccae21c9da3585f4172a3703ac77f28da20
Leonardo <leonardo@pache.eng.br>
2019-05-21 00:41:50 +0200

    fb register

--- account.manager/src/main/java/es/pache/spring/account/manager/controller/AccountController.java
+++ account.manager/src/main/java/es/pache/spring/account/manager/controller/AccountController.java
@@ -5,13 +5,13 @@ package es.pache.spring.account.manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.User;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import es.pache.spring.account.manager.controller.requests.RegisterFormRequest;
 import es.pache.spring.account.manager.controller.response.ProfileResponse;
@@ -32,23 +32,32 @@ public class AccountController {
 	@Autowired
 	FacebookService fbService;
 
-	@RequestMapping(value="/fbAutho")
-	public String createFacebookAuthorization() {
-		//redirect to url generated!!
-		return fbService.createFacebookAuthorizationURL();
+	@RequestMapping(value = "/fbAutho")
+	public ModelAndView createFacebookAuthorization() {
+		return new ModelAndView("redirect:" + fbService.createFacebookAuthorizationURL());
 	}
-	
-	@RequestMapping(value="/register-facebook")
-	public void registerWithFacebook(@RequestParam("code") String code) {
+
+	@RequestMapping(value = "/register-facebook")
+	public String registerWithFacebook(@RequestParam("code") String code) {
 		Facebook fbProfile = fbService.createFacebookAccessToken(code);
+
+		String[] fields = { "locale", "location", "languages", "age_range", "hometown", "first_name", "last_name",
+				"middle_name", "name", "picture", "email" };
+		// "id", "about", , , "context", "cover", "currency", "devices", "education",
+		// "email",
+		// "favorite_athletes", "favorite_teams", "first_name", "gender",
+		// "inspirational_people", "installed", "install_type",
+		// "is_verified", "languages", "last_name", "link", "locale", "location",
+		// "meeting_for", "middle_name", "name", "name_format",
+		// "political", "quotes", "payment_pricepoints", "relationship_status",
+		// "religion", "security_settings", "significant_other",
+		// "sports", "test_group", "timezone", "third_party_id", "updated_time",
+		// "verified", "video_upload_limits", "viewer_can_send_gift",
+		// "website", "work"
 		
-		String[] fields = {"id"};
 		String str = fbProfile.fetchObject("me", String.class, fields);
-		
-		fbProfile.getBaseGraphApiUrl();
-		
-		
-//		fbProfile.userOperations().PROFILE_FIELDSgetUserProfile();
+
+		// fbProfile.userOperations().PROFILE_FIELDS
 		// Profile p = new Profile();
 		// p.setFirstName(details.get("given_name"));
 		// p.setLastName(details.get("family_name"));
@@ -56,6 +65,7 @@ public class AccountController {
 		// p.setAvatar(details.get("picture"));
 		// p = profileRepository.save(p);
 		// return ProfileResponse.toResponse(p);
+		return str;
 	}
 
 	// @RequestMapping(value = "/home")
